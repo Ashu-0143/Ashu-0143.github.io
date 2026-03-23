0514278aec25e7f99041f508949434a8dcb4c1d5
Ashu <ashokboxer21006@gmail.com>
2026-03-23 18:09:32 +0530

    v1.1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function(){
+
   $(".game").on("click",function(){
     $(".game").toggle();
     $(this).toggle("fast");
-    $("").
+    $(this).append("<h3>Good Luck !</h3>")
   });
 });
