5736cf82f37ca4ffdf727778c719ca8ee3b2dae3
Ashu <ashokboxer21006@gmail.com>
2026-03-23 18:11:25 +0530

    modfied js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
   $(".game").on("click",function(){
     $(".game").toggle();
     $(this).toggle("fast");
-    $(this).append("<h3>Good Luck !</h3>")
+    
   });
 });
