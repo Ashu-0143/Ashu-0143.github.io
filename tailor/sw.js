a914d44b46c0ef7c2f12119f690831e566bd011f
Ashu <ashokboxer21006@gmail.com>
2026-04-05 21:43:58 +0530

    modified paths

--- tailor/sw.js
+++ tailor/sw.js
@@ -1,27 +1,23 @@
-const CACHE_NAME = "tailor-app-v1";
+const CACHE_NAME = "tailor-app-v2";
 
 const urlsToCache = [
-  "/tailor/",
-  "/tailor/index.html",
-  "/tailor/home.html",
-  "/tailor/daily.html",
-  "/tailor/customers.html",
-  "/tailor/summary.html",
-  "/tailor/manifest.json"
+  "./",
+  "./index.html",
+  "./home.html",
+  "./daily.html",
+  "./customers.html",
+  "./summary.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
